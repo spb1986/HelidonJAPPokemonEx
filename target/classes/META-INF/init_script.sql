64dfb3db629c46694e5954fc10557e030b218829
spb1986 <109908194+spb1986@users.noreply.github.com>
2022-07-24 20:16:00 +0530

    Add files via upload

---
+++ target/classes/META-INF/init_script.sql
@@ -0,0 +1,6 @@
+INSERT INTO POKEMON VALUES ('1',10,'Caterpie','Bug');
+INSERT INTO POKEMON VALUES ('2',1,'Bulbasaur','Grass');
+INSERT INTO POKEMON VALUES ('3',8,'Wartortle','Water');
+INSERT INTO POKEMON VALUES ('4',87,'Dewgong','Ice');
+INSERT INTO POKEMON VALUES ('5',16,'Pidgey','Normal');
+INSERT INTO POKEMON VALUES ('6',5,'Charmeleon','Fire');
